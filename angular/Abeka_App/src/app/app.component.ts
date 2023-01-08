040e9f33a9725dada824cb9fb3ed0518f8b14a9e
Esteban <estedcg27@gmail.com>
2023-01-07 22:27:58 -0400

    upload

--- angular/Abeka_App/src/app/app.component.ts
+++ angular/Abeka_App/src/app/app.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello! I am an Angular application 🔥</h1>
+  <!-- header -->
+  <app-header></app-header>
+  
+  <!-- routes get injected here -->
+  <router-outlet></router-outlet>
 
-    <router-outlet></router-outlet>
+  <!-- footer -->
+  <app-footer></app-footer>
   `,
   styles: []
 })
---
+++ angular/Abeka_App/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  <div class="navbar is-dark">
+        <div class="navbar-brand">
+            <a class="navbar-item">
+              <img src="assets/imgs/abeka_logo.png">
+            </a>
+        </div>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent {
+
+}
